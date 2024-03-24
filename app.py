cffd22f0cfb5e80ec9553729812dce4d372608d6
Sunil Devaman Undare <79266873+undaresunil@users.noreply.github.com>
2024-03-24 15:08:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -46,11 +46,11 @@ def diet():
     ]
 
     carbs_sources = [
-        {'name': 'Brown Rice', 'protein': 5, 'carbs': 45},
+        {'name': 'Brown Rice', 'protein': 5, 'carbs': 25},
         {'name': 'Quinoa', 'protein': 4, 'carbs': 21},
         {'name': 'Sweet Potato', 'protein': 2, 'carbs': 20},
-        {'name': 'Oats', 'protein': 13, 'carbs': 68},
-        {'name': 'Whole Grain Bread', 'protein': 8, 'carbs': 49},
+        {'name': 'Oats', 'protein': 13, 'carbs': 28},
+        {'name': 'Whole Grain Bread', 'protein': 8, 'carbs': 29},
         {'name': 'Bananas', 'protein': 1, 'carbs': 27},
         {'name': 'Apples', 'protein': 0, 'carbs': 25},
         {'name': 'Oranges', 'protein': 1, 'carbs': 21},
@@ -70,8 +70,8 @@ def diet():
     ]
 
     # Define the daily protein and carbs requirements
-    daily_protein_requirement = data['Protien_intake']  # in grams
-    daily_carbs_requirement = data['Carbs_intake']  # in grams
+    daily_protein_requirement = int(data['Protien_intake'])  # in grams
+    daily_carbs_requirement = int(data['Carbs_intake'])  # in grams
 
     # Define the time intervals for meals
     meal_timetable = {
@@ -89,24 +89,24 @@ def diet():
     carbs_counts = {food['name']: 0 for food in carbs_sources}
     selected_protein_today = {}
     selected_carbs_today = {}
-
     Timetable_dict={}
     # Generate the weekly timetable
     start_date = datetime.today()
-    end_date = start_date + timedelta(days=(data['week']*7))  # One week
+    end_date = start_date + timedelta(days=int(data['week'])*7)  # One week
 
     current_date = start_date
     while current_date <= end_date:
-        print(f"Diet Plan for {current_date.strftime('%A, %Y-%m-%d')}:")
-        # print(Timetable_dict)
-        # Reset counts at the start of each day
-        protein_counts = {food['name']: 0 for food in protein_sources}
-        carbs_counts = {food['name']: 0 for food in carbs_sources}
-        selected_protein_today = {}
-        selected_carbs_today = {}
-        
-        total_protein_intake = 0
-        total_carbs_intake = 0
+        # print(f"Diet Plan for {current_date.strftime('%A, %Y-%m-%d')}:")
+        Timetable_dict[current_date.strftime('%A, %Y-%m-%d')]={}
+        # Reset counts at the start of each week
+        if current_date.weekday() == 0:  # Monday
+            protein_counts = {food['name']: 0 for food in protein_sources}
+            carbs_counts = {food['name']: 0 for food in carbs_sources}
+            selected_protein_today = {}
+            selected_carbs_today = {}
+            # Reshuffle food items at the start of each week
+            random.shuffle(protein_sources)
+            random.shuffle(carbs_sources)
         
         for meal, timing in meal_timetable.items():
             # print(f"{meal} ({timing['start']} - {timing['end']}):")
@@ -115,38 +115,74 @@ def diet():
             selected_protein = None
             while not selected_protein:
                 selected_protein = random.choice(protein_sources)
-                if protein_counts[selected_protein['name']] >= 2:
+                if selected_protein['name'] in selected_protein_today.values() or protein_counts[selected_protein['name']] >= 2:
                     selected_protein = None
             
             selected_protein_today[meal] = selected_protein['name']
             protein_counts[selected_protein['name']] += 1
 
-            # Calculate protein intake amount for the current meal
-            remaining_protein = daily_protein_requirement - total_protein_intake
-            protein_intake_amount = min(remaining_protein, selected_protein['protein'])
-            total_protein_intake += protein_intake_amount
+            protein_intake_amount = daily_protein_requirement * (selected_protein['protein'] / sum(food['protein'] for food in protein_sources))
             
             # Select a carbs source for the current mealtime
             selected_carbs = None
             
             while not selected_carbs:
                 selected_carbs = random.choice(carbs_sources)
-                if carbs_counts[selected_carbs['name']] >= 2:
+                if selected_carbs['name'] in selected_carbs_today.values() or carbs_counts[selected_carbs['name']] >= 2:
                     selected_carbs = None
             
             selected_carbs_today[meal] = selected_carbs['name']
             carbs_counts[selected_carbs['name']] += 1
 
-            # Calculate carbs intake amount for the current meal
-            remaining_carbs = daily_carbs_requirement - total_carbs_intake
-            carbs_intake_amount = min(remaining_carbs, selected_carbs['carbs'])
-            total_carbs_intake += carbs_intake_amount
-            Timetable_dict[current_date.strftime('%A, %Y-%m-%d')]={'Protiein':selected_protein['name'],'Carbs':selected_carbs['name']}
-        
+            carbs_intake_amount = daily_carbs_requirement * (selected_carbs['carbs'] / sum(food['carbs'] for food in carbs_sources))
+            
+            # print(f"\t- Protein Source: {selected_protein['name']}: {protein_intake_amount:.2f} grams")
+            # print(f"\t- Carbs Source: {selected_carbs['name']}: {carbs_intake_amount:.2f} grams")
+            Timetable_dict[current_date.strftime('%A, %Y-%m-%d')][meal]={"protein":selected_protein['name'],"carbs":selected_carbs['name']}
         current_date += timedelta(days=1)
+
+    # Mapping protein values
+    for date_key, meals in Timetable_dict.items():
+        for meal_key, nutrients in meals.items():
+            print(nutrients)
+            print(meal_key)
+            protein_name = nutrients['protein']
+            for source in protein_sources:
+                if source['name'] == protein_name:
+                    nutrients['protein'] = {'name': protein_name, 'protein': source['protein']}
+            
+            carbs_name = nutrients['carbs']
+            for source in carbs_sources:
+                if source['name'] == carbs_name:
+                    nutrients['carbs'] = {'name': carbs_name, 'carbs': source['carbs']}
+                if source['name'] == carbs_name:
+                    nutrients['carbs'] = {'name': carbs_name, 'carbs': source['carbs']}
+    for i,v in Timetable_dict.items():
+        tot1=[]
+        tot=[]
+        for j,k in v.items():
+            tot.append(k['protein']['protein'])
+            tot1.append(k['carbs']['carbs'])
+        sum1=sum(tot)
+        sum2=sum(tot1)
+        print(f"tot:{tot}, tot1:{tot1}")
+        print(f"sum1:{sum1}, sum2:{sum2}")
+        Diff_protein=100-sum1
+        Diff_carbs=100-sum2
+        New_p=round(Diff_protein/len(v), 2)
+        New_c=round(Diff_carbs/len(v), 2)
+        print(f"Diff_protein:{Diff_protein}, Diff_carbs:{Diff_carbs}")
+        print(f"New_p:{New_p}, New_c:{New_c}")
+        print("==================================================")
+        for i,v in Timetable_dict.items():
+            for j,k in v.items():
+                k['protein']['protein'] += New_p
+                k['carbs']['carbs'] += New_c
+        
+    print(Timetable_dict)
 
     return jsonify({'data': Timetable_dict})
 
 # Run Server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
